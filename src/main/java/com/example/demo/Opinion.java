3ed81b6da6867562ce2bc9ed345da5d1ff4f4f47
Michał Nowakowski <michal951@poczta.onet.pl>
2019-12-19 11:54:12 +0100

    add mysql with certain connection data

--- src/main/java/com/example/demo/Opinion.java
+++ src/main/java/com/example/demo/Opinion.java
@@ -16,7 +16,7 @@ public class Opinion {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(columnDefinition = "LONGVARCHAR")
+    @Column(columnDefinition = "LONGTEXT")
     private String review;
     private String nickname;
     private int grade;
