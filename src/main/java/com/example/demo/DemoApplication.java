ce861557bb9fe1db75c06c32c4b9512d3f862a32
Michał Nowakowski <michal951@poczta.onet.pl>
2019-12-29 20:53:25 +0100

    refactor in order of deploy

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,9 +3,10 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 
     @Autowired
     OpinionRepository opinionRepository;
